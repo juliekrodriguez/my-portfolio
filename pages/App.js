20925ec7d3b436a310b39a38201f73f223e96060
julie <julie.rodriguez1@wsu.edu>
2022-11-25 17:13:12 -0800

    first commit

---
+++ pages/App.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import ContactForm from "./contact";
+
+function App() {
+  useEffect(() => {
+    if (document) {
+      const stylesheet = document.createElement("link");
+      stylesheet.rel = "stylesheet";
+      stylesheet.href =
+        "https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css";
+
+      document.head.appendChild(stylesheet);
+    }
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="py-6">
+          <ContactForm />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
